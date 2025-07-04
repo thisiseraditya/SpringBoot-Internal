3f751326018b06b38d834db74b0f70883c02acd2
Aditya Kumar <150783601+thisiseraditya@users.noreply.github.com>
2025-07-04 19:02:47 +0530

    Delete internship-tracker/internship-tracker directory

--- internship-tracker/internship-tracker/src/main/java/com/example/internship_tracker/repository/ApplicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.internship_tracker.repository;
-import com.example.internship_tracker.model.Application;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface ApplicationRepository extends JpaRepository<Application, Long>{
-}
